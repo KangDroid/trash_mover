f9ebb36b5c232e09b724176cf5994b618030e124
KangDroid <hyunwoo286@gmail.com>
2020-12-11 16:02:09 +0900

    Initial File

---
+++ remove_to_trash.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <unistd.h>
+
+using namespace std;
+
+#define USERNAME_LIMIT 1024
+
+class UserDefinition {
+public:
+    UserDefinition() {
+        this->is_recursive_delete = false;
+        this->is_verbose = false;
+        this->is_force = false;
+    }
+    void setRecursive(bool val) {this->is_recursive_delete = val;}
+    void setForce(bool val) {this->is_force = val;}
+    void setVerbose(bool val) {this->is_verbose = val;}
+
+    bool isRecursive() {return this->is_recursive_delete;}
+    bool isForce() {return this->is_force;}
+    bool isVerbose() {return this->is_verbose;}
+private:
+    bool is_recursive_delete;
+    bool is_force;
+    bool is_verbose;
+};
+
+string get_usr_name() {
+    char* username_arr = new char[USERNAME_LIMIT];
+    if (getlogin_r(username_arr, USERNAME_LIMIT) != 0) {
+        perror("Error occured when getting username");
+    }
+    string ret_val = string(username_arr);
+    delete[] username_arr;
+    return ret_val;
+}
+
+string remove_newline(string& target) {
+    string ret_val = "";
+    for (int i = 0; i < target.length(); i++) {
+        if (target.at(i) != '\n') {
+            ret_val += target.at(i);
+        }
+    }
+    return ret_val;
+}
+
+void move_to_trash(filesystem::path& target, UserDefinition& udf) {
+    filesystem::path destination_target;
+    // Get username
+    string username_str = get_usr_name();
+    filesystem::path trash_path("/Users/"+username_str+"/.Trash");
+    if (!filesystem::exists(trash_path)) {
+        cerr << "No such file or directory: " << trash_path.string() << endl;
+        cerr << "Contact Developer with log" << endl;
+    }
+
+    // Default Target directory
+    destination_target = trash_path / target.filename();
+
+    // Scan trash if same filename exists.
+    for (auto& p : filesystem::directory_iterator(trash_path)) {
+        if (target.filename() == p.path().filename()) {
+            time_t cur_time = time(NULL);
+            string tim_str = string(ctime(&cur_time));
+            tim_str = remove_newline(tim_str);
+            // In this case, rename target one with appending deletion date.
+            destination_target = trash_path / (target.filename().string() + " " + tim_str);
+        }
+    }
+
+    // if (udf.isVerbose()) {
+        cout << filesystem::absolute(target) << " --> " << destination_target << endl;
+    // }
+    filesystem::rename(filesystem::absolute(target), destination_target);
+}
+
+/**
+ * Supported Options
+ * -r : Recursive
+ * -f : Force
+ * -v : verbose
+ */
+int main(int argc, char** argv) {
+    UserDefinition usr_de;
+    if (argc < 2) {
+        cerr << "Needs at least one argument to delete some files!" << endl;
+        return -1;
+    }
+
+    char c;
+    while ((c = getopt(argc, argv, "rvf")) != -1) {
+        switch(c) {
+            case 'r':
+                usr_de.setRecursive(true);
+            break;
+            case 'f':
+                usr_de.setForce(true);
+            break;
+            case 'v':
+                usr_de.setVerbose(true);
+            break;
+            case '?':
+                cerr << "Unknown Argument" << endl;
+            break;
+        }
+    }
+    
+    // Parse
+    for (int i = 1; i < argc; i++) {
+        if (string(argv[i]).at(0) == '-') {
+            continue;
+        } else {
+            string path_tmp = string(argv[i]);
+            filesystem::path current_path(path_tmp);
+            if (!filesystem::exists(current_path)) {
+                cerr << "Path: " << current_path << endl;
+            } else {
+                // move to trash
+                move_to_trash(current_path, usr_de);
+            }
+        }
+    }
+    return 0;
+}
